6d6d62b793090e5a8518e334825ed646c542c040
Onkar Pandit <pandit.onkar9@gmail.com>
2018-06-23 11:13:16 +0530

    Update block.test.js

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,5 +1,6 @@
 const Block = require('./block');
 
+
 describe('Block', () => {
   let data, lastBlock, block;
 
@@ -15,5 +16,17 @@ describe('Block', () => {
 
   it('sets the `lastHash` to match the hash of the last block', () => {
     expect(block.lastHash).toEqual(lastBlock.hash);
+  });
+
+  it('generates a hash that matches the difficulty', () => {
+    expect(block.hash.substring(0, block.difficulty)).toEqual('0'.repeat(block.difficulty));
+  });
+
+  it('lowers the difficulty for slowly mined blocks',()=>{
+    expect(Block.adjustDifficulty(block , block.timestamp+360000)).toEqual(block.difficulty-1);
+  });
+
+  it('raises the difficulty for quickly mined blocks',()=>{
+    expect(Block.adjustDifficulty(block , block.timestamp+1)).toEqual(block.difficulty+1);
   });
 });
